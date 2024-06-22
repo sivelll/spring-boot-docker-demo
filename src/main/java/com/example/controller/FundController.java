e7333801ee1824141030cef2f8e8417d817b1089
Andersen Lee <andersenlee@fstop.com.tw>
2024-06-23 01:02:14 +0800

    at latest
    at latest

--- src/main/java/com/example/controller/FundController.java
+++ src/main/java/com/example/controller/FundController.java
@@ -3,16 +3,20 @@ package com.example.controller;
 import com.example.service.FundService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.http.*;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @Api
 public class FundController {
     @Autowired
     private FundService fundService;
+
+    private RestTemplate restTemplate;
 
     @PostMapping("/fetch-fund-data")
     public String fetchFundData() {
@@ -23,5 +27,24 @@ public class FundController {
             e.printStackTrace();
             return "Error occurred while fetching data!";
         }
+    }
+
+    @PostMapping("/fetch-fund-data2")
+    public ResponseEntity<String> fetchFundData2() {
+
+        final String apiUrl = "https://www.cathaybk.com.tw/cathaybk/service/newwealth/fund/chartservice.asmx/GetFundNavChart";
+        String requestBody = "{\"req\":{\"Keys\":[\"10480016\"],\"From\":\"2023/03/10\",\"To\":\"2024/03/\"}}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+
+        // 發送POST請求並取得回應
+        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.POST, entity, String.class);
+
+        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+
+
     }
 }
